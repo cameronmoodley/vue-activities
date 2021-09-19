653e06f4279e8851728adf8c9ee9dc3f7389cfef
Cameron Moodley <moodley.cameron@gmail.com>
2021-09-19 12:52:07 +0200

    added in categories mock data function

--- src/App.vue
+++ src/App.vue
@@ -1,234 +1,211 @@
 <template>
-	<div id="activityApp">
-		<nav class="navbar is-white topNav">
-			<div class="container">
-				<div class="navbar-brand">
-					<h1>Activity Planner</h1>
-				</div>
-			</div>
-		</nav>
-		<nav class="navbar is-white">
-			<div class="container">
-				<div class="navbar-menu">
-					<div class="navbar-start">
-						<a class="navbar-item is-active" href="#">
-							Newest
-						</a>
-						<a class="navbar-item" href="#">
-							In Progress
-						</a>
-						<a class="navbar-item" href="#">Finished</a>
-					</div>
-				</div>
-			</div>
-		</nav>
-		<section class="container">
-			<div class="columns">
-				<div class="column is-3">
-					<a
-						v-if="!isFormDisplayed"
-						@click="toggleFormDisplay"
-						class="button is-primary is-block is-alt is-large"
-						href="#"
-					>
-						New Activity
-					</a>
-					<div v-if="isFormDisplayed" class="create-form">
-						<h2>Create Activity</h2>
-						<form>
-							<div class="field">
-								<label class="label">Title</label>
-								<div class="control">
-									<input
-										v-model="newActivity.title"
-										class="input"
-										type="text"
-										placeholder="Read a Book"
-									/>
-								</div>
-							</div>
-							<div class="field">
-								<label class="label">Notes</label>
-								<div class="control">
-									<textarea
-										v-model="newActivity.notes"
-										class="textarea"
-										placeholder="Write some notes here"
-									></textarea>
-								</div>
-							</div>
-							<div class="field is-grouped">
-								<div class="control">
-									<button @click="createActivity" class="button is-link">
-										Create Activity
-									</button>
-								</div>
-								<div class="control">
-									<button class="button is-text" @click="toggleFormDisplay">
-										Cancel
-									</button>
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
-				<div class="column is-9">
-					<div class="box content">
-						<ActivityItem
-							v-for="activity in activities"
-							:key="activity.id"
-							:activity="activity"
-						/>
-					</div>
-				</div>
-			</div>
-		</section>
-	</div>
+  <div id="activityApp">
+    <nav class="navbar is-white topNav">
+      <div class="container">
+        <div class="navbar-brand">
+          <h1>Activity Planner</h1>
+        </div>
+      </div>
+    </nav>
+    <nav class="navbar is-white">
+      <div class="container">
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item is-active" href="#">
+              Newest
+            </a>
+            <a class="navbar-item" href="#">
+              In Progress
+            </a>
+            <a class="navbar-item" href="#">Finished</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <section class="container">
+      <div class="columns">
+        <div class="column is-3">
+          <a
+            v-if="!isFormDisplayed"
+            class="button is-primary is-block is-alt is-large"
+            href="#"
+            @click="toggleFormDisplay"
+          >
+            New Activity
+          </a>
+          <div v-if="isFormDisplayed" class="create-form">
+            <h2>Create Activity</h2>
+            <form>
+              <div class="field">
+                <label class="label">Title</label>
+                <div class="control">
+                  <input
+                    v-model="newActivity.title"
+                    class="input"
+                    type="text"
+                    placeholder="Read a Book"
+                  />
+                </div>
+              </div>
+              <div class="field">
+                <label class="label">Notes</label>
+                <div class="control">
+                  <textarea
+                    v-model="newActivity.notes"
+                    class="textarea"
+                    placeholder="Write some notes here"
+                  ></textarea>
+                </div>
+              </div>
+              <div class="field is-grouped">
+                <div class="control">
+                  <button class="button is-link" @click="createActivity">
+                    Create Activity
+                  </button>
+                </div>
+                <div class="control">
+                  <button class="button is-text" @click="toggleFormDisplay">
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="column is-9">
+          <div class="box content">
+            <ActivityItem
+              v-for="activity in activities"
+              :key="activity.id"
+              :activity="activity"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import ActivityItem from '@/components/ActivityItem';
-import { fetchActivities } from '@/api';
+import ActivityItem from '@/components/ActivityItem'
+import { fetchActivities, fetchCategories, fetchUsers } from '@/api'
 export default {
-	name: 'App',
-	components: { ActivityItem },
-	data() {
-		return {
-			isFormDisplayed: false,
-			message: 'Hello Vue!',
-			titleMessage: 'Title Message Vue!!!!!',
-			isTextDisplayed: true,
-			newActivity: {
-				title: '',
-				notes: '',
-			},
-			items: { 1: { name: 'Filip' }, 2: { name: 'John' } },
-			user: {
-				name: 'Filip Jerga',
-				id: '-Aj34jknvncx98812',
-			},
-			activities: {},
-			categories: {
-				'1546969049': { text: 'books' },
-				'1546969225': { text: 'movies' },
-			},
-		};
-	},
-	beforeCreate() {
-		console.log('beforeCreate called!');
-	},
-	created() {
-		this.activities = fetchActivities();
-	},
-	beforeMount() {
-		console.log('beforeMount called!');
-	},
-	mounted() {
-		console.log('mounted called!');
-	},
-	beforeUpdate() {
-		console.log('beforeUpdate called!');
-	},
-	updated() {
-		console.log('updated called!');
-	},
-	beforeDestroy() {
-		console.log('beforeDestroy called!');
-	},
-	destroyed() {
-		console.log('destroyed called!');
-	},
+  name: 'App',
+  components: { ActivityItem },
+  data() {
+    return {
+      isFormDisplayed: false,
+      message: 'Hello Vue!',
+      titleMessage: 'Title Message Vue!!!!!',
+      isTextDisplayed: true,
+      newActivity: {
+        title: '',
+        notes: ''
+      },
+      items: { 1: { name: 'Filip' }, 2: { name: 'John' } },
+      user: {},
+      activities: {},
+      categories: {}
+    }
+  },
+  created() {
+    this.activities = fetchActivities()
+    this.categories = fetchCategories()
+    this.users = fetchUsers()
 
-	methods: {
-		toggleTextDisplay() {
-			this.isTextDisplayed = !this.isTextDisplayed;
-		},
-		toggleFormDisplay() {
-			this.isFormDisplayed = !this.isFormDisplayed;
-		},
-		createActivity() {
-			console.log(this.newActivity);
-		},
-	},
-};
+    console.log(this.users)
+    console.log(this.categories)
+  },
+  methods: {
+    toggleTextDisplay() {
+      this.isTextDisplayed = !this.isTextDisplayed
+    },
+    toggleFormDisplay() {
+      this.isFormDisplayed = !this.isFormDisplayed
+    },
+    createActivity() {
+      console.log(this.newActivity)
+    }
+  }
+}
 </script>
 
 <style>
 #activityApp {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
 }
 
 html,
 body {
-	font-family: 'Open Sans', serif;
-	background: #f2f6fa;
+  font-family: 'Open Sans', serif;
+  background: #f2f6fa;
 }
 footer {
-	background-color: #f2f6fa !important;
+  background-color: #f2f6fa !important;
 }
 
 .example-wrapper {
-	margin-left: 30px;
+  margin-left: 30px;
 }
 
 .topNav {
-	border-top: 5px solid #3498db;
+  border-top: 5px solid #3498db;
 }
 .topNav .container {
-	border-bottom: 1px solid #e6eaee;
+  border-bottom: 1px solid #e6eaee;
 }
 .container .columns {
-	margin: 3rem 0;
+  margin: 3rem 0;
 }
 .navbar-menu .navbar-item {
-	padding: 0 2rem;
+  padding: 0 2rem;
 }
 aside.menu {
-	padding-top: 3rem;
+  padding-top: 3rem;
 }
 aside.menu .menu-list {
-	line-height: 1.5;
+  line-height: 1.5;
 }
 aside.menu .menu-label {
-	padding-left: 10px;
-	font-weight: 700;
+  padding-left: 10px;
+  font-weight: 700;
 }
 .button.is-primary.is-alt {
-	background: #00c6ff;
-	background: -webkit-linear-gradient(to bottom, #0072ff, #00c6ff);
-	background: linear-gradient(to bottom, #0072ff, #00c6ff);
-	font-weight: 700;
-	font-size: 14px;
-	height: 3rem;
-	line-height: 2.8;
+  background: #00c6ff;
+  background: -webkit-linear-gradient(to bottom, #0072ff, #00c6ff);
+  background: linear-gradient(to bottom, #0072ff, #00c6ff);
+  font-weight: 700;
+  font-size: 14px;
+  height: 3rem;
+  line-height: 2.8;
 }
 .media-left img {
-	border-radius: 50%;
+  border-radius: 50%;
 }
 .media-content p {
-	font-size: 14px;
-	line-height: 2.3;
-	font-weight: 700;
-	color: #8f99a3;
+  font-size: 14px;
+  line-height: 2.3;
+  font-weight: 700;
+  color: #8f99a3;
 }
 article.post {
-	margin: 1rem;
-	padding-bottom: 1rem;
-	border-bottom: 1px solid #e6eaee;
+  margin: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #e6eaee;
 }
 article.post:last-child {
-	padding-bottom: 0;
-	border-bottom: none;
+  padding-bottom: 0;
+  border-bottom: none;
 }
 .menu-list li {
-	padding: 5px;
+  padding: 5px;
 }
 
 .navbar-brand > h1 {
-	font-size: 31px;
-	padding: 20px;
+  font-size: 31px;
+  padding: 20px;
 }
 </style>
--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,34 +1,34 @@
 <template>
-	<article class="post">
-		<h4>{{ activity.title }}</h4>
-		<div class="media">
-			<div class="media-left">
-				<p class="image is-32x32">
-					<img src="../assets/user.png" />
-				</p>
-			</div>
-			<div class="media-content">
-				<div class="content">
-					<p>
-						<a href="#">Cameron Moodley</a> updated
-						{{ activity.updatedAt }} minutes ago &nbsp;
-					</p>
-				</div>
-			</div>
-			<div class="media-right">
-				<span>Progress Bar Here</span>
-			</div>
-		</div>
-	</article>
+  <article class="post">
+    <h4>{{ activity.title }}</h4>
+    <div class="media">
+      <div class="media-left">
+        <p class="image is-32x32">
+          <img src="../assets/user.png" />
+        </p>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <a href="#">Cameron Moodley</a> updated
+            {{ activity.updatedAt }} minutes ago &nbsp;
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <span>Progress Bar Here</span>
+      </div>
+    </div>
+  </article>
 </template>
 <script>
 export default {
-	props: {
-		activity: {
-			type: Object,
-			required: true,
-		},
-	},
-};
+  props: {
+    activity: {
+      type: Object,
+      required: true
+    }
+  }
+}
 </script>
 <style lang=""></style>
